1f8e064d9128f2f1c231bec8f25f625fc2954dfe
Sutan Mufti <sutan@enums.id>
2026-01-15 21:12:09 +0700

    accodreon

--- src/components/Panel.svelte
+++ src/components/Panel.svelte
@@ -1,5 +1,9 @@
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button/index.js";
+</script>
+
 <div
   class="max-w-80 bg-white w-80 h-fit max-h-100 overflow-auto rounded text-sm p-1"
 >
-  hohoho
+  <Button>Wawawa</Button>
 </div>
