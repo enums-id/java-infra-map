fd37b0d73d538a74cd840169721a3986f0e09238
Sutan Mufti <sutan@enums.id>
2026-01-16 08:17:19 +0700

    rm comment

--- src/appMain.svelte.ts
+++ src/appMain.svelte.ts
@@ -88,52 +88,6 @@ function registerBaseMap(map: mapboxgl.Map) {
     },
   });
 
-  // map.addLayer({
-  //   id: "pelabuhan",
-  //   type: "circle",
-  //   source: "pelabuhan",
-  //   "source-layer": "pelabuhan",
-  //   filter: [
-  //     "all",
-  //     ["!=", ["get", "hirarki_p"], "Belum Terdefinisi"],
-  //     ["!=", ["get", "hirarki_p"], "Pelabuhan Lokal"],
-  //   ],
-  //   paint: {
-  //     "circle-color": [
-  //       "match",
-  //       ["get", "hirarki_p"],
-
-  //       "Pelabuhan Utama",
-  //       "#99023e",
-  //       "Pelabuhan Pengumpan Regional",
-  //       "#6a006e",
-  //       "Pelabuhan Pengumpan",
-  //       "#6e4900",
-
-  //       /* fallback */
-  //       "#cccccc",
-  //     ],
-
-  //     "circle-radius": [
-  //       "match",
-  //       ["get", "hirarki_p"],
-
-  //       "Pelabuhan Utama",
-  //       10,
-  //       "Pelabuhan Pengumpan Regional",
-  //       8,
-  //       "Pelabuhan Pengumpan",
-  //       6,
-
-  //       /* fallback */
-  //       5,
-  //     ],
-
-  //     "circle-stroke-width": 1,
-  //     "circle-stroke-color": "#ffffff",
-  //   },
-  // });
-
   map.addLayer({
     id: "Pelabuhan Pengumpan",
     type: "circle",
