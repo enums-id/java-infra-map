5a99e00f2c00bce7ea1e6b40472cc9b39af90418
Sutan Mufti <sutan@enums.id>
2026-01-19 15:48:28 +0700

    footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -5,7 +5,7 @@
   import Theme from "./Theme.svelte";
 </script>
 
-<div class="text-xs flex justify-between py-1">
+<div class="text-xs flex justify-between py-1 items-center">
   <div class="px-4">
     <span
       >{appState.mapzoom[0].toFixed(2)}, {appState.mapzoom[0].toFixed(8)}, {appState.mapzoom[0].toFixed(
@@ -14,7 +14,7 @@
     >
   </div>
   <div class="flex gap-1 items-center">
-    <div class="mx-2">
+    <div class="mx-2 border rounded p-2 border-accent">
       <Label for="highlight">
         <Checkbox id="highlight" bind:checked={appState.highlightFeature} />
         Highlight
