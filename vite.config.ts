9ebb4230df755e7d7d4e096b84cf2cdc24fc4dcb
Sutan Mufti <sutan@enums.id>
2026-01-23 10:30:04 +0700

    adds commit version

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
+import { execSync } from "node:child_process";
+const commitHash = execSync("git rev-parse --short HEAD").toString().trim();
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte(), tailwindcss()],
@@ -10,5 +13,8 @@ export default defineConfig({
     alias: {
       $lib: path.resolve("./src/lib"),
     },
+  },
+  define: {
+    __GIT_COMMIT__: JSON.stringify(commitHash),
   },
 });
